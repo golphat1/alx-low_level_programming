d5907b0d8937064204e762c3e303999392ecba52
golphat1 <golphat@yahoo.com>
2022-12-08 04:39:32 -0800

    assembler

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#inlude <studio.h>
+
+/**
+ * main - Entry point
+ *
+ * *Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	puts ("\"Programming is like building a multilingual puzzle")
+		return (0);
+
+}
