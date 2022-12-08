f1be05341d3c2986ca3f7564ac1217dc20b41143
golphat1 <golphat@yahoo.com>
2022-12-08 13:59:28 -0800

    hello puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,14 @@
-#inlude <studio.h>
+#include <stdio.h>
 
- /**
- *main - Entry point
- *
- *Return: Always 0 (success)
+/**
+ * main - Entry
+ 
+ *Return: Always 0
  */
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-		return (0);
+	return (0);
 
 }
