2cbc5a89729df4efd56a3ab26d128b8dd54818b5
golphat1 <golphat@yahoo.com>
2022-12-09 08:50:18 -0800

    basically simple operating system

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,11 @@
-#include <stdio.h>
-#include <unistd.h>                                                                                                                      
-/** 
- * main - Entry
+#include <unistd.h>
+/**
+ * main - prints "and that piece of art is useful" - Dora Korpar, 2015-10-19; 
+ * followed by a new line, to standard error
  * Return: Always 1
  */
 int main(void)
 {
-	write(1, "and that piece of art is useful" - Dora Korpar, 2015-10-19);
+	write(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",59);
 	return (1);
 }
