50cdce6f9d2329226862fdf6d7966421aee91d3a
golphat1 <golphat@yahoo.com>
2022-12-11 13:58:01 -0800

    task2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - print alphabets in lowercase
+ * Return: Always 0
+ */
+int main(void)
+{
+	char lc;
+
+	for (lc = 'a'; lc <= 'z'; lc++)
+	{
+		putchar(lc)
+	}
+	putchar('\n');
+	return (0);
