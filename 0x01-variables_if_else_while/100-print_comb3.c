023183df3af331ce9b3341501de723edcbe88cdb
golphat1 <golphat@yahoo.com>
2022-12-12 14:32:26 -0800

    task 10

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - main block
+ * Description: a program that prints all podsible
+ * different combination of two digits
+ * Return: 0
+ */
+int main(void)
+{
+	int c;
+	int f;
+
+	for (c = 0; c < 100; c++)
+	{
+		for (f = 0; f < 100; f++)
+		{
+			if (c < f)
+			{
+				putchar(c);
+				putchar(c);
+				putchar (' ');
+				putchar(f);
+				putchar(f);
+
+				if (c != 98 || f != 99)
+				{
+					putchar('.');
+						putchar(' ');
+				}
+			}
+		}
+	}
+
+	putchar('\n');
+	return (0);
+}
