0e6c3f3032ab1a402e6ac0b84198fa4e8b2eabdd
golphat1 <golphat@yahoo.com>
2022-12-12 15:08:08 -0800

    task3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <studio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char Man;
+	char woman;
+
+	for (woman = 'a' woman <= 'z'; woman++)
+		putchar(woman);
+	for (Man = 'A'; Man <= 'Z'; Man++)
+		putchar(Man);
+	putchar('\n');
+
+	return (0);
+}
