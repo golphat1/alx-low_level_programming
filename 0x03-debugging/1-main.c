d86d3a0821619987506efedcce29c070cdc2e2fa
golphat1 <golphat@yahoo.com>
2022-12-16 06:26:33 -0800

    task1

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -8,7 +8,6 @@
 int main(void)
 {
 	int i;
-
 	printf("Infinite loop incoming :(\n");
 
 	i = 0;
@@ -18,7 +17,7 @@ int main(void)
 		putchar(i);
 	}
 
-	printf("Infinite loop avoided! \\o/\n");
+	printf("Infinite loop avoided! \o/\n");
 
 	return (0);
 }
