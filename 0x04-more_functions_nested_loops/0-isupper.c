dcd455695e479a12275782c9b9f43b1a4def1da4
golphat1 <golphat@yahoo.com>
2022-12-17 06:25:40 -0800

    task0

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,8 +2,8 @@
 
 /**
  * _isupper - checks if parameter is an uppercase character.
- * @c: input character
- * Return: 1 if is an uppercase character, 0 in other case.
+ * @c: c - Variable
+ * Return: Always 0.
  */
 int _isupper(int c)
 {
