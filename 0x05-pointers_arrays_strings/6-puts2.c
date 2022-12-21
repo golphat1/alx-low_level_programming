b0fd93b540d8fa13e47628d891c9480aa3f9a104
golphat1 <golphat@yahoo.com>
2022-12-20 17:58:48 -0800

    task7

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -7,16 +7,14 @@
  */
 void puts2(char *str)
 {
-	int i;
+	int i = 0;
 
-	i = 0;
-	while (str[i] != '\0')
+	for (str[i] != '\0'; i++);
 	{
-		if (i % 2 == 0)
-		{
+		if ((i % 2) == 0)
 			_putchar(str[i]);
-		}
-		i++;
+		else
+			continue;
 	}
 	_putchar('\n');
 }
