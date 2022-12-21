ba8276925668a8fff63752ea6dd3a5fa7ebf5cbd
golphat1 <golphat@yahoo.com>
2022-12-20 17:25:00 -0800

    task5

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,17 +1,19 @@
 #include "main.h"
 
 /**
- * _puts - function that prints a string
+ * _puts - function that prints a string, followed by a new line, to
+ * stdout
+ *
  * @str: string input
- * Reurn: string
+ *
  */
 void _puts(char *str)
 {
-	int i;
+	int index;
 
-	for (i = 0; str[i] != '\0'; i++)
+	for (index = 0; str[index] != '\0'; index++)
 	{
-		_putchar(str[i]);
+	_putchar(str[index]);
 	}
 	_putchar('\n');
 }
---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_rev - function thatprints a string in reverse
+ * fcounter is to first coount to end, n is to count back
+ * @s: str input
+ * Return: string in reverse
+ */
+void print_rev(char *s)
+{
+	int fcounter = 0;
+	int i, n;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		fcounter++;
+	}
+	
+	for (n = (fcounter - 1); n >= 0; n--)
+	{
+		_putchar(s[n]);
+	}
+	_putchar('\n');
+}
