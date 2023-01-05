9e8d5b1a06b8d835197f4202d548d3f79d4e9416
golphat1 <golphat@yahoo.com>
2023-01-05 09:46:14 -0800

    task 3

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -9,13 +9,16 @@
  */
 int factorial(int n)
 {
-	int result = n;
-
 	if (n < 0)
+	{
 		return (-1);
-
-	else if (n >= 0 && n <= 1)
+	}
+	else if (n == 0)
+	{
 		return (1);
+	}
+	else
+	{
+		return (n * factorial(n - 1));
+	}
 }
-
-
