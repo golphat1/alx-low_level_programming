96e0c9b36967d6175c96b892d4449edfeed182dd
golphat1 <golphat@yahoo.com>
2023-01-10 07:25:03 -0800

    task1

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -5,10 +5,16 @@
  * @c: input
  * Return: 1 if c is a digit, 0 otherwise
  */
-int _issdigit(int c)
+int _isdigit(int c)
 {
-	if ((c >= 48) && (c <= 57))
+	if (c >= '0' && c <= '9')
+	{
 		return (1);
+	}
 	else
+	{
 		return (0);
+	}
 }
+
+
