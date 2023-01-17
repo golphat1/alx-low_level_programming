509d4d9c6b25b54df6d34441ce985608d8bf88a2
golphat1 <golphat@yahoo.com>
2023-01-17 12:02:00 -0800

    task 0

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,4 +1,3 @@
-#ifndef DOG_H
 #define DOG_H
 
 /**
@@ -13,15 +12,3 @@ struct dog
 	float age;
 	char *owner;
 };
-
-/**
- * dog_t - Type for struct dog
- */
-typedef struct dog dog_t;
-
-void int_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct dog *d);
-dog_t *new_dog(char *name, float age, char *owner);
-void free_dog(dog_t *d)
-
-#endif /*DOG_H*/
