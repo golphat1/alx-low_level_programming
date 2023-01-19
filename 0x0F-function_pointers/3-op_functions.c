48c4a53a49e7a522784f476b20089175d6e483b3
golphat1 <golphat@yahoo.com>
2023-01-19 11:49:30 -0800

    task 3

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,85 @@
+#include "3-calc.h"
+#include <stddef.h>
+
+/**
+ * op_add - returns the sum of a and b
+ * @a: first integer
+ * @b: second integer
+ * Return: sum of a and b
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - returns the difference of a and b
+ * @a: first integer
+ * @b: second integer
+ * Return: difference of a and b
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - returns the product of a and b
+ * @a: first integer
+ * @b: second integer
+ * Return: product of a and b
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - returns the result of the division of a by b
+ * @a: first integer
+ * @b: second integer
+ * Return: result of the division of a by b
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - returns the remainder of the division of a by b
+ * @a: first integer
+ * @b: second integer
+ * Return: remainder of the division of a by b
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
+
+/**
+ * get_op_func - selects the correct function to perform the operation
+ * @s: operator passed as argument to the program
+ * Return: pointer to the function that corresponds to the operator
+ */
+int (*get_op_func(char *s))(int, int)
+{
+	op_t ops[] =
+	{
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	int i;
+	i = 0;
+	while (i < 5)
+	{
+		if (*s == *(ops[i].op))
+			return (ops[i].f);
+		i++;
+	}
+	return (NULL);
+}
+
